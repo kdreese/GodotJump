773326304b7cc91e0121a79669c5aeca42b18bb6
Terry Hearst <thearst3rd@gmail.com>
2021-12-02 21:41:29 -0500

    Screen wrap

--- objects/Jumper/Jumper.gd
+++ objects/Jumper/Jumper.gd
@@ -7,6 +7,9 @@ const BOUNCE_SPEED := -450.0
 
 var _velocity := Vector2(0, 0)
 
+onready var extents := ($CollisionShape2D.shape as RectangleShape2D).extents
+onready var bounds := get_viewport_rect().size
+
 
 func _ready() -> void:
 	pass
@@ -18,3 +21,8 @@ func _physics_process(delta: float) -> void:
 	_velocity = move_and_slide(_velocity, Vector2.UP)
 	if is_on_floor():
 		_velocity.y = BOUNCE_SPEED
+
+	if position.x < -extents.x:
+		position.x += bounds.x + (2 * extents.x)
+	if position.x >= bounds.x + extents.x:
+		position.x -= bounds.x + (2 * extents.x)
