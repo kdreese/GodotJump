9fdbd66bba4e100773496ca9ee85399df3d002b3
Kyle Reese <reesekyle1998@gmail.com>
2021-12-07 21:21:04 -0500

    Added springs

--- objects/Jumper/Jumper.gd
+++ objects/Jumper/Jumper.gd
@@ -5,6 +5,7 @@ const MOVE_SPEED = 150.0
 const GRAVITY := 350.0
 const BOUNCE_SPEED := -450.0
 const JETPACK_DURATION := 2.0
+const SPRING_SPEED := -900.0
 
 var _velocity := Vector2(0, 0)
 var jetpack_timer := 0.0
@@ -33,7 +34,10 @@ func _physics_process(delta: float) -> void:
 		jetpack_timer -= delta
 	_velocity = move_and_slide(_velocity, Vector2.UP)
 	if is_on_floor():
-		_velocity.y = BOUNCE_SPEED
+		if "Spring" in get_last_slide_collision().collider.get_filename():
+			_velocity.y = SPRING_SPEED
+		else:
+			_velocity.y = BOUNCE_SPEED
 
 	if position.x < -extents.x:
 		position.x += bounds.x + (2 * extents.x)
