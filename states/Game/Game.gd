c2906eb0a2c8fc680dd57bf28481f3b96be8f080
Terry Hearst <thearst3rd@gmail.com>
2022-01-13 21:42:23 -0500

    Randomly offset platforms on the x axis

--- states/Game/Game.gd
+++ states/Game/Game.gd
@@ -5,13 +5,14 @@ const Platform = preload("res://objects/Platform/Platform.tscn")
 
 const PLATFORM_OFFSCREEN_OFFSET := 100.0
 const PLATFORM_SPAWN_DISTANCE := 200.0
+const PLATFORM_RANGE = 250.0
 
-onready var next_platform_spawn_y := ($Jumper as Node2D).position.y + 100.0
+onready var next_platform_spawn_y := ($Platforms/FirstPlatform as Node2D).position.y - PLATFORM_SPAWN_DISTANCE
 onready var camera := ($Jumper/Camera2D as Camera2D)
 
 
 func _ready() -> void:
-	pass
+	randomize()
 
 
 func _physics_process(_delta: float) -> void:
@@ -24,7 +25,7 @@ func _physics_process(_delta: float) -> void:
 			platform.queue_free()
 
 	while view_top_y - PLATFORM_OFFSCREEN_OFFSET < next_platform_spawn_y:
-		spawn_platform(Vector2(view_size.x / 2, next_platform_spawn_y))
+		spawn_platform(Vector2(view_size.x / 2 + rand_range(-PLATFORM_RANGE, PLATFORM_RANGE), next_platform_spawn_y))
 		next_platform_spawn_y -= PLATFORM_SPAWN_DISTANCE
 
 
