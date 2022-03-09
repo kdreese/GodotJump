1a705586a480119c6f118b052fefbb41fdc58ba6
Terry Hearst <thearst3rd@gmail.com>
2022-03-08 21:07:18 -0500

    Add keyboard shortcut to toggle full screen

--- states/MainMenu/Global.gd
+++ states/MainMenu/Global.gd
@@ -2,3 +2,15 @@ extends Node
 
 
 var high_score := 0
+
+
+func _ready() -> void:
+	set_pause_mode(PAUSE_MODE_PROCESS)
+
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("toggle_fullscreen"):
+		if OS.get_name() == "HTML5":
+			return
+		OS.window_fullscreen = not OS.window_fullscreen
+		get_tree().set_input_as_handled()
